80fc31e91cb5d8c628fced1798bd588618507f4f
Amanda Zambelli <amandazambelli@ymail.com>
2022-05-19 19:53:09 -0300

    requisitos 7 e 8

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-class Wallet extends React.Component {
+class Table extends React.Component {
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
       <div>
-        Table
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          { expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ Number(expense.value).toFixed(2) }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+              <td>
+                { (Number(expense.value)
+                * Number(expense.exchangeRates[expense.currency].ask)).toFixed(2) }
+              </td>
+              <td>Real</td>
+              <td><button type="button">Excluir</button></td>
+            </tr>
+          ))}
+        </table>
       </div>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (globalState) => ({
+  expenses: globalState.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf.isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
